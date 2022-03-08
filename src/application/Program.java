aad7c4f8849590733b58b6b78c5c50a8594f3342
jpnxhko <fernando.h.k.ogata@gmail.com>
2022-03-08 15:33:44 -0300

    Projeto Criado

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ public class Program {
 
     public static void main(String[] args) {
 
+        System.out.println("Teste");
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
